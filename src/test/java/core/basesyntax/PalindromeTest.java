43eca4be98de3338882b0a3471ec47bd0a97bd4d
Pavel Kurilyuk <50627236+Tarabarnik@users.noreply.github.com>
2020-01-24 14:28:11 +0200

    updated test to not allow students to delete numbers with regular exp… (#59)
    * updated test to not allow students to delete numbers with regular expressions
    
    * fixed comments
    
    * fixed tests naming
    
    * fixed last test naming

--- src/test/java/core/basesyntax/PalindromeTest.java
+++ src/test/java/core/basesyntax/PalindromeTest.java
@@ -6,34 +6,45 @@ import org.junit.Test;
 public class PalindromeTest {
     private static final String LETTERS_PALINDROME = "Madam, I'm Adam!";
     private static final String LETTERS_AND_NUMBERS_PALINDROME = "1.) level-1";
+    private static final String LETTERS_AND_NUMBERS_NON_PALINDROME = ".) level-1";
     private static final String NUMBERS_PALINDROME = "123 43 21";
     private static final String NON_PALINDROME = "Nice day!";
 
+
     @Test
-    public void isPalindromeWithAllLetters() {
+    public void isPalindrome_lettersPalindrome() {
         Palindrome palindrome = new Palindrome();
         boolean actualResult = palindrome.isPalindrome(LETTERS_PALINDROME);
-        Assert.assertTrue("Test faid with text " + LETTERS_PALINDROME, actualResult);
+        Assert.assertTrue("Test failed with text " + LETTERS_PALINDROME, actualResult);
     }
 
     @Test
-    public void isPalindromeWithLettersAndNumbers() {
+    public void isPalindrome_lettersAndNumbers() {
         Palindrome palindrome = new Palindrome();
         boolean actualResult = palindrome.isPalindrome(LETTERS_AND_NUMBERS_PALINDROME);
-        Assert.assertTrue("Test faid with text " + LETTERS_AND_NUMBERS_PALINDROME, actualResult);
+        Assert.assertTrue("Test failed with text " + LETTERS_AND_NUMBERS_PALINDROME,
+                actualResult);
     }
 
     @Test
-    public void isPalindromeWithNumbers() {
+    public void isPalindrome_lettersAndNumbersNegativeCase() {
+        Palindrome palindrome = new Palindrome();
+        boolean actualResult = palindrome.isPalindrome(LETTERS_AND_NUMBERS_NON_PALINDROME);
+        Assert.assertFalse("Test failed with text " + LETTERS_AND_NUMBERS_NON_PALINDROME +
+                "  We shouldn't remove numbers from string", actualResult);
+    }
+
+    @Test
+    public void isPalindrome_numbers() {
         Palindrome palindrome = new Palindrome();
         boolean actualResult = palindrome.isPalindrome(NUMBERS_PALINDROME);
-        Assert.assertTrue("Test faid with text " + NUMBERS_PALINDROME, actualResult);
+        Assert.assertTrue("Test failed with text " + NUMBERS_PALINDROME, actualResult);
     }
 
     @Test
-    public void isPalindrome() {
+    public void isPalindrome_negativeCaseWithoutNumbers() {
         Palindrome palindrome = new Palindrome();
         boolean actualResult = palindrome.isPalindrome(NON_PALINDROME);
-        Assert.assertFalse("Test faid with text " + NON_PALINDROME, actualResult);
+        Assert.assertFalse("Test failed with text " + NON_PALINDROME, actualResult);
     }
 }
